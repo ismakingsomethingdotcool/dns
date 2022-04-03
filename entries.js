cd049c4b0ad7db5ac45e4b7973f652d29ca238f6
William Horning <wgyt735yt@gmail.com>
2022-04-02 23:03:02 -0400

    fix entries file to fix syntax error

--- entries.js
+++ entries.js
@@ -1,5 +1,5 @@
 D_EXTEND(
-  'ismakingsomething.cool',
+  'ismakingsomething.cool'
   // hey there! if you want to add a domain to the list, read the contributers.md file
   // example: NS('william', 'ns1.packetframe.com.')
 );
