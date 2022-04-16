24a9a50ef64e5177410c220d890cacfef10c5802
William Horning <wgyt735yt@gmail.com>
2022-04-16 12:13:08 -0400

    forgot the dot

--- entries.js
+++ entries.js
@@ -1,7 +1,7 @@
 D_EXTEND(
   'ismakingsomething.cool',
-  NS('kagl','ns1.kagl.me'),
-  NS('kagl','ns2.kagl.me')
+  NS('kagl','ns1.kagl.me.'),
+  NS('kagl','ns2.kagl.me.')
   // hey there! if you want to add a domain to the list, read the contributers.md file
   // example: NS('william', 'ns1.packetframe.com.')
 );
